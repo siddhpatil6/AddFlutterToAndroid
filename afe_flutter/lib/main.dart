2b17afaa663f14ad5c3063ea0f56d31832e3cd4e
siddhantp <siddhant@greenlightplanet.com>
2020-06-20 10:27:29 +0530

    second commit

--- afe_flutter/lib/main.dart
+++ afe_flutter/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
@@ -44,16 +47,17 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  static const String _channel = 'com.example.android.MainActivity';
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  static const platform = const MethodChannel(_channel);
+
+  _getNewActivity() async {
+    setState(() async {
+      try {
+      await platform.invokeMethod('startNewActivity');
+    } on PlatformException catch (e) {
+      print(e.message);
+    }});
   }
 
   @override
@@ -100,9 +104,8 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+      floatingActionButton: RaisedButton(
+        onPressed: _getNewActivity,
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
